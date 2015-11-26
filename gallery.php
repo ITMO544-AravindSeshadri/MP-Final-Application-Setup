abf8aef7876584e4b8c1b28ef27e885ce6d24a46
Aravind Seshadri <aravsesh1@gmail.com>
2015-11-25 19:39:28 -0600

    Adding session logic

--- gallery.php
+++ gallery.php
@@ -55,8 +55,9 @@
 
 <?php
 session_start();
-$email = $_POST["email"];
-echo $email;
+$sessionvar = $_SESSION["mailid"];
+#echo $sessionvar;
+
 require 'vendor/autoload.php';
 
 use Aws\Rds\RdsClient;
@@ -81,13 +82,32 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
-//below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
+
+
+if(strlen($sessionvar)==0)
+{
 $link->real_query("SELECT * FROM MP1");
+#$link->real_query("SELECT * FROM MP1");
 $res = $link->use_result();
 #echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
     echo "<img src =\" " . $row['RawS3URL'] . "\" />";
 # echo $row['ID'] . "Email: " . $row['email'];
+}
+$link->close();
+}
+else
+{
+//below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
+#$link->real_query("SELECT * FROM MP1");
+$link->real_query("SELECT * FROM MP1 where email='$sessionvar'");
+#$link->real_query("SELECT * FROM MP1");
+$res = $link->use_result();
+#echo "Result set order...\n";
+while ($row = $res->fetch_assoc()) {
+    echo "<img src =\" " . $row['RawS3URL'] . "\" />";
+# echo $row['ID'] . "Email: " . $row['email'];
+}
 }
 $link->close();
 ?>
